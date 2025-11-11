7be7c148f34b56194a21188651062ae3a0b74bfe
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-11 11:56:39 +0100

    create a html code and map listfilm in it

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import { useState, useEffect } from "react";
 
 export default function App() {
 
-  return (
+  const listFilms = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
 
+  const [films, setFilms] = useState(listFilms)
+
+  return (
+    <div className="container">
+      <input type="search" />
+
+      <ul>
+        {films.map(film => (
+          <li key={film.title} className="list-group-item">{film.title}</li>
+        ))}
+      </ul>
+
+    </div>
   );
 }
