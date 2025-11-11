8d0ce3b239509009091b0306e52e3ff9130225f4
stefanobellomo <stefanobellomo1998@gmail.com>
2025-11-11 16:46:59 +0100

    create new useeffects for new if condition and create a form for add new film and genre

--- src/App.jsx
+++ src/App.jsx
@@ -16,66 +16,81 @@ export default function App() {
   const [newFilms, setNewFilms] = useState('')
   const [newGenre, setNewGenre] = useState('')
   const [stateFilms, setStateFilms] = useState(films)
+  const [genre, setGenre] = useState('')
 
   useEffect(() => {
-    const filtered = films.filter(film => film.title.toLowerCase().includes(search.toLowerCase()))
+    const filtered = listFilms.filter(film => film.title.toLowerCase().includes(search.toLowerCase()))
     setStateFilms(filtered)
 
     if (search === '') {
-      setStateFilms(listFilms) // perchè voglio che ritorni intero
+      setStateFilms(films) // perchè voglio che ritorni intero
     }
 
   }, [films, search])
 
+  useEffect(() => {
+    if (genre === '') {
+      setFilms(listFilms)
+    } else {
+      const filtered = listFilms.filter(film => film.genre === genre)
+      setFilms(filtered)
+    }
+  }, [genre])
 
-  const filteredGenre = []
+
+  const genres = []
   for (let i = 0; i < listFilms.length; i++) {
     const film = listFilms[i];
 
-    if (!filteredGenre.includes(film.genre)) {
-      filteredGenre.push(film.genre)
+    if (!genres.includes(film.genre)) {
+      genres.push(film.genre)
     }
   }
 
-  // function handleSubmit(e) {
-  //   e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault()
 
-  //   if (newFilms.length < 4 && newGenre.length < 4) {
-  //     setNewFilms('')
-  //     setNewGenre('')
-  //   } else {
-  //     const film = {
-  //       title: newFilms,
-  //       genre: newGenre,
-  //     }
-  //     console.log(film);
-
-  //   }
-  // }
+    if (newFilms.length < 4 && newGenre.length < 4) {
+      setNewFilms('')
+      setNewGenre('')
+    } else {
+      const film = {
+        title: newFilms,
+        genre: newGenre,
+      }
+      const addNewFilm = [...listFilms, film]
+      setFilms(addNewFilm)
+      setNewFilms('')
+      setNewGenre('')
+    }
+  }
 
 
   return (
     <div className="container">
 
-      {/* 
+
       <form onSubmit={handleSubmit}>
         <input type="text" value={newFilms} placeholder="add new title film" onChange={(e) => setNewFilms(e.target.value)} />
-        <button className="badge bg-success my-4 mx-4" type="submit">Add</button> 
+        <button className="badge bg-success my-4 mx-4" type="submit">Add</button>
         <input type="text" value={newGenre} placeholder="add genre film" onChange={(e) => setNewGenre(e.target.value)} />
         <button className="badge bg-success my-4 mx-4" type="submit">Add</button>
       </form>
-      */}
+
 
       <input type="search" value={search} onChange={(e) => setSearch(e.target.value)} />
 
-      <select className="form-select" aria-label="Default select example">
+      <div className="py-3">
+        <select className="form-select" value={genre} onChange={(e) => setGenre(e.target.value)}>
+          <option value=''>scegli un genere</option>
 
-        <option value='genre'>Genere</option>
-        {filteredGenre.map(genre => (
-          <option key={genre} value={genre}>{genre}</option>
-        ))}
+          {genres.map(opt => (
+            <option key={opt} value={opt}>{opt}</option>
+          ))}
 
-      </select>
+        </select>
+      </div>
+
 
       <ul>
         {stateFilms.map(film => (
